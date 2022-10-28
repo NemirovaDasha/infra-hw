0357990b629dbb593ac980f6b2c689fca65d7d2d
d_nemirova <kovalewa.d@gmail.com>
2022-10-28 16:19:02 +0300

    test ts with error action

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const testing = (text) =>{
-    return text + "!"
-}
-
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
