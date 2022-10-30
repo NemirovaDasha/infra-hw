ba57ef60371054a057326fd028045ab79adf01a3
d_nemirova <kovalewa.d@gmail.com>
2022-10-30 19:09:33 +0300

    test request to tracker

--- actions/build-release.sh
+++ actions/build-release.sh
@@ -4,19 +4,16 @@
 
 # запись коммитов в тикет
 CURRENT_TAG=$GITHUB_REF_NAME
-TAG_NUM=${GITHUB_REF_NAME:7} #последняя цифра релиза
-PREV_TAG_NUM=$(expr $TAG_NUM - 1) #последняя цифра предыдущего релиза
-PREV_TAG="rc-0.0.$PREV_TAG_NUM" #предыдущий релиз
+PREV_TAG=$(git describe --abbrev=0 --tags $CURRENT_TAG^) #предыдущий релиз
 
-IS_PREV_TAG=$(git tag | grep "rc-0.0.$PREV_TAG_NUM")
 echo "$CURRENT_TAG"
 echo "$PREV_TAG"
 
-if [[ -n $IS_PREV_TAG ]]; then
+if [[ $PREV_TAG ]]; then
   echo "$CURRENT_TAG and $PREV_TAG"
   # берем коммиты между двумя
   RESULT=$(git log $CURRENT_TAG...$PREV_TAG --pretty=format:"%h %an %s")
-  else
+else
   # берем все коммиты
   RESULT=$(git log $CURRENT_TAG --pretty=format:"%h %an %s")
 fi
