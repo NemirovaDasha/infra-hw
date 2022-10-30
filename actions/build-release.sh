b9832c7ac4cb7cf10b33311a8df29adef1a3b67a
d_nemirova <kovalewa.d@gmail.com>
2022-10-30 17:28:31 +0300

    test run build

--- actions/build-release.sh
+++ actions/build-release.sh
@@ -4,7 +4,7 @@
 
 #RESULT=$(git log --pretty=format:"%h\%an\%s;")
 #echo ${RESULT}
-API_TRACKER = https://api.tracker.yandex.net/v2/myself
+API_TRACKER = "https://api.tracker.yandex.net/v2/myself"
 
 CURRENT_TAG=$GITHUB_REF_NAME
 TAG_NUM=${GITHUB_REF_NAME:7} #последняя цифра релиза
@@ -15,7 +15,7 @@ IS_PREV_TAG=$(git tag | grep "rc-0.0.$PREV_TAG_NUM")
 echo "$CURRENT_TAG"
 echo "$PREV_TAG"
 
-curl -X GET $API_TRACKER -H "Authorization: OAuth $OAUTH_TOKEN" -H "X-Org-ID: $X_ORG_ID"
+curl -X GET "$API_TRACKER" -H "Authorization: OAuth $OAUTH_TOKEN" -H "X-Org-ID: $X_ORG_ID"
 
 #if [[ -n $IS_PREV_TAG ]]; then
   #берем коммиты между двумя
