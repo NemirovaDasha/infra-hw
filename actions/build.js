e7eed640b38a788ef3cfbcabf506bb32bc37617a
d_nemirova <kovalewa.d@gmail.com>
2022-10-30 21:25:18 +0300

    test commit

--- actions/build.js
+++ actions/build.js
@@ -1,19 +1,12 @@
-console.log(process.env)
-process.exit(0);
-try {
-  const OAUTH_TOKEN = process.env.OAUTH_TOKEN;
-  const X_ORG_ID = process.env.X_ORG_ID;
-  const TAG = process.env.GITHUB_REF_NAME;
-  const PREV_TAG = process.env.PREV_TAG;
+const OAUTH_TOKEN = process.env.OAUTH_TOKEN;
+const X_ORG_ID = process.env.X_ORG_ID;
+const TAG = process.env.GITHUB_REF_NAME;
+const PREV_TAG = process.env.PREV_TAG;
 
-  const ticketTitle = `Релиз ${TAG} - ${new Date().toLocaleDateString()}`;
+const ticketTitle = `Релиз ${TAG} - ${new Date().toLocaleDateString()}`;
 
-  console.log(OAUTH_TOKEN)
-  console.log(X_ORG_ID)
-  console.log(TAG)
-  console.log(PREV_TAG)
-  console.log(ticketTitle)
-
-} catch (error) {
-  console.error("error");
-}
+console.log(OAUTH_TOKEN)
+console.log(X_ORG_ID)
+console.log(TAG)
+console.log(PREV_TAG)
+console.log(ticketTitle)
