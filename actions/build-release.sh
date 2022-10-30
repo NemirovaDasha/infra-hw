9d9dfec18dd403a3f8c3ae0590361d2fb989ff30
d_nemirova <kovalewa.d@gmail.com>
2022-10-30 15:02:35 +0300

    test run build

--- actions/build-release.sh
+++ actions/build-release.sh
@@ -2,7 +2,13 @@
 
 # сборка проекта
 
-#RESULT=$(git log --pretty=format:"%h %an %s")
-#echo ${RESULT}
+RESULT=$(git log --pretty=format:"%h\%an\%s;")
+echo ${RESULT}
+
+echo 'ref'
+echo ${{ github.ref_name }}
+
+echo 'sha'
+echo ${{ github.ref_name }}
 
 exit 0
